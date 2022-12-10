0329c8ffda4f473f8cb50690560278c2cb8f8c71
Gunnar Johansson <johansson.gunnar89@gmail.com>
2022-12-10 13:26:54 +0100

    Add Day04.

---
+++ Day04/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("../../../input.txt");
+            List<int[]> ranges = new();
+            int fullyContainedRanges = 0;
+            int overlappingRanges = 0;
+
+            for (int i1 = 0; i1 < input.Length; i1++)
+            {
+                string line = input[i1];
+                string[] rangeStrings = line.Split(',');
+                string[] range1String = rangeStrings[0].Split('-');
+                int[] range1 = new int[int.Parse(range1String[1])
+                    - int.Parse(range1String[0])
+                    + 1];
+
+                for (int i = 0; i < range1.Length; i++)
+                {
+                    range1[i] = int.Parse(range1String[0]) + i;
+                }
+
+                string[] range2String = rangeStrings[1].Split('-');
+                int[] range2 = new int[int.Parse(range2String[1]) - int.Parse(range2String[0]) + 1];
+
+                for (int i = 0; i < range2.Length; i++)
+                {
+                    range2[i] = int.Parse(range2String[0]) + i;
+                }
+
+                ranges.Add(range1);
+                ranges.Add(range2);
+
+                if (
+                    (range1[0] <= range2[0]
+                     && range1[^1] >= range2[^1]) || (range2[0] <= range1[0] && range2[^1] >= range1[^1])
+                )
+                {
+                    fullyContainedRanges++;
+                }
+
+                if (
+                    range1[0] <= range2[^1] && range1[^1] >= range2[0]
+                )
+                {
+                    overlappingRanges++;
+                }
+            }
+
+            Console.WriteLine(fullyContainedRanges
+                + "\r\n"
+                + overlappingRanges);
+            Console.ReadKey();
+        }
+    }
+}
